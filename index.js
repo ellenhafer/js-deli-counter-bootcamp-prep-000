70ebdcd14cbe5fa7bf073e6c58510b1ef06ea0d6
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-27 12:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function takeANumber(katzDeliLine, name) {
 }
 
 function nowServing() {
-  if(katzDeliLine === []){
+  if(katzDeliLine.length === 0){
     return "There is nobody waiting to be served!"
   }
   else {
