cfdeed2215790f185d05f7b81308e779cde32c4d
Ellen O'Neill <ellen.o.neill.munro@gmail.com>
2018-05-27 12:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name) {
-  return `${name}, you are number ${katzDeliLine.length + 1} in line.`
+  return `Welcome, ${name}. You are number ${katzDeliLine.length + 1} in line.`
 }
